e12a8162e68a283bb98b8c489346d343e962b4fc
Algent <algent@gmail.com>
2016-04-30 15:25:02 +0200

    Fixed javascript error if undefined class.

--- js/main.js
+++ js/main.js
@@ -331,6 +331,9 @@ var stateKey = 'darksouls3_state';
 
     function canFilter(entry) {
         var regexFilter = new RegExp('^f_(.*)');
+        if (entry.hasClass('') === true) {
+            return true;
+        }
         var classList = entry.attr('class').split(/\s+/);
         for (var i = 0; i < classList.length; i++) {
             if (!classList[i].match(regexFilter)) {
